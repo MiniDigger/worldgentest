5d0688884a3d71454523cf6385b93315dd6effc3
MiniDigger <admin@minidigger.me>
2019-02-09 15:46:31 +0100

    port to new paper api

--- src/main/java/me/minidigger/worldgentest/PlusGenerator.kt
+++ src/main/java/me/minidigger/worldgentest/PlusGenerator.kt
@@ -1,11 +1,8 @@
 package me.minidigger.worldgentest
 
-import com.sun.org.apache.xpath.internal.operations.Plus
-import net.minecraft.server.v1_13_R2.*
 import org.bukkit.Location
 import org.bukkit.Material
 import org.bukkit.World
-import org.bukkit.craftbukkit.v1_13_R2.CraftWorld
 import org.bukkit.generator.ChunkGenerator
 import java.util.*
 
@@ -13,44 +10,22 @@ class PlusGenerator(id: String) : ChunkGenerator() {
 
     private val size = id.split("-")[1].toInt()
 
-    private lateinit var overworldGen : ChunkProviderGenerate
-
-    fun getNmsGen(world : World): ChunkProviderGenerate {
-        val nmsWorld = (world as CraftWorld).handle
-        val genSettings = GeneratorSettingsOverworld()
-        val biomeProvider = WorldChunkManagerOverworld(BiomeLayoutOverworldConfiguration())
-        // todo looks like we need to set worlddata before being able to use biome layout overworld config
-        val overworldGen = ChunkProviderGenerate(nmsWorld, biomeProvider, genSettings)
-        return overworldGen
-    }
-
     override fun generateChunkData(world: World, random: Random?, x: Int, z: Int, biome: ChunkGenerator.BiomeGrid): ChunkGenerator.ChunkData {
-        val chunkData = createChunkData(world)
+        return if (shouldFillChunk(x, z)) {
+            // main content
 
-
-        if (shouldFillChunk(x, z)) {
-            // main content //TODO delegate to vanilla chunk gen
-//            chunkData.setRegion(0, 63, 0, 16, 64, 16, Material.GRASS_BLOCK)
-//            chunkData.setRegion(0, 1, 0, 16, 63, 16, Material.DIRT)
-//            chunkData.setRegion(0, 0, 0, 16, 1, 16, Material.BEDROCK)
-
-            if(!::overworldGen.isInitialized) {
-                overworldGen = getNmsGen(world)
-            }
-
-            val chunkPrimer = ProtoChunk(ChunkCoordIntPair(x, z), ChunkConverter.a) // empty chunk converter
-            overworldGen.a(x, z, chunkPrimer)
+            val chunkData = createVanillaChunkData(world, x , z)
 
             // borders
-            when {
-                !shouldFillChunk(x + 1, z) -> chunkData.setRegion(15, 0, 0, 16, 256, 16, Material.BEDROCK)
-                !shouldFillChunk(x, z + 1) -> chunkData.setRegion(0, 0, 15, 16, 256, 16, Material.BEDROCK)
-                !shouldFillChunk(x - 1, z) -> chunkData.setRegion(0, 0, 0, 1, 256, 16, Material.BEDROCK)
-                !shouldFillChunk(x, z - 1) -> chunkData.setRegion(0, 0, 0, 16, 256, 1, Material.BEDROCK)
-            }
-        }
+            if (!shouldFillChunk(x + 1, z)) chunkData.setRegion(15, 0, 0, 16, 256, 16, Material.BEDROCK)
+            if (!shouldFillChunk(x, z + 1)) chunkData.setRegion(0, 0, 15, 16, 256, 16, Material.BEDROCK)
+            if (!shouldFillChunk(x - 1, z)) chunkData.setRegion(0, 0, 0, 1, 256, 16, Material.BEDROCK)
+            if (!shouldFillChunk(x, z - 1)) chunkData.setRegion(0, 0, 0, 16, 256, 1, Material.BEDROCK)
 
-        return chunkData
+            chunkData
+        } else {
+            createChunkData(world) // return empty
+        }
     }
 
     override fun canSpawn(world: World, x: Int, z: Int) = shouldFillChunk(x, z)
@@ -63,4 +38,10 @@ class PlusGenerator(id: String) : ChunkGenerator() {
 
         return (x < size && z < size * 2) || (x < size * 2 && z < size)
     }
+
+    override fun shouldAddVanillaDecorations() = true
+
+    override fun shouldAddVanillaFeatures() = true
+
+    override fun shouldAddVanillaMobs() = true
 }
